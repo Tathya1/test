7281e3b8e3d9d87e9fe7114c82ff444dfeb09e53
Tathya1 <tathya.vaghasia@students.iiit.ac.in>
2025-01-06 06:34:56 +0000

    mex.cpp

---
+++ tle/mex.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+ll xor_upto(ll n)
+        {
+            if (n % 4 == 0)
+                return n;
+            else if (n % 4 == 1)
+                return 1;
+            else if (n % 4 == 2)
+                return n + 1;
+            else
+                return 0;
+        }
+
+int main()
+{
+    ll t, a, b;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a >> b;
+        ll zor = xor_upto(a-1);
+        // cout << zor << " ";
+        // cout << (zor^b) << " ";
+        if (zor == b)
+        {
+            cout << a << endl;
+        }
+        else
+        {
+            if ((b^zor) == a)
+                cout << a + 2 << endl;
+            else
+                cout << a + 1 << endl;
+        }
+    }
+    return 0;
+}
